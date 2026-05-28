955442a6763c0132803cfcaf9e2c6586af22c87d
DLQQ35 <architkhanna260912@gmail.com>
2026-05-28 20:42:15 +0530

    user_login_app

--- user_login_app/main.py
+++ user_login_app/main.py
@@ -39,3 +39,45 @@ def logout():
     id = ''
     msg = 'You have been logged out!'
     return render_template('login.html', msg=msg, name='name', id='id')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        mydb = mysql.connector.connect(
+                    host="remotemysql.com",
+                    user="wXy63pU5g1",
+                    password="qVOgRNMGRK",
+                    database="wXy63pU5g1"
+        )
+
+        mycursor = mydb.cursor()
+        print(username)
+        mycursor.execute('SELECT * FROM LoginDetails WHERE username = %s AND email = %s', (username, email))
+        account = mycursor.fetchone()
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers!'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form!'
+        else:
+            mycursor.execute('INSERT INTO LoginDetails VALUES (%s, %s, %s)', (username, password, email))
+            mydb.commit()
+            msg = 'You have successfully registered!'
+            name = username
+            return render_template('login.html', msg=msg, name=name)
+    elif request.method == 'POST':
+        msg = 'Please fill out the form!'
+    return render_template('register.html', msg=msg)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('login.html')
+
+app.run(host='0.0.0.0', port=8080)
